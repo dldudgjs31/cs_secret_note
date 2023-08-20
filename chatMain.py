305034314ed6542dff89ef8a07c7ce538076e47d
dldudgjs31 <dldudgjs31@naver.com>
2023-08-20 23:52:24 +0900

    최종 반영(쿼리메이커_adv 수정)

--- chatMain.py
+++ chatMain.py
@@ -1,5 +1,7 @@
 from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtCore import *
+from PyQt6.QtGui import QStandardItemModel
+
 import chatgpt
 import openai
 from PyQt6.QtWidgets import QPushButton, QMessageBox, QRadioButton, QTableWidgetItem
@@ -18,13 +20,28 @@ class gpt():
 
     #아이디 중복여부 체크
     id_use_yn = True
-    # 현재 로그인 중인 계정
+
+    # 현재 로그인 중인 계정 정보
     current_user = ''
+
+    #이메일 인증
     email=''
     authKey = ''
     verification_code = ''
+    email_valid = False
 
     # db 공통 connection 연결
+    db_host = ''
+    db_user = ''
+    db_pwd = ''
+    db_database = ''
+    def dbConnect_queryadv(self,host,user,pwd,database):
+        # 데이터베이스 연결
+        connection = pymysql.connect(host=host,
+                                     user=user,
+                                     password=pwd,
+                                     database=database)
+        return connection
     def dbConnect(self):
         host = "my8002.gabiadb.com"
         user = "amsdb"
@@ -109,6 +126,12 @@ class gpt():
             msgBox.setText("인증번호을 입력해주세요.")
             msgBox.exec()
             return
+        #이메일 인증 체크
+        if self.email_valid == False:
+            msgBox = QMessageBox()
+            msgBox.setText("이메일 인증을 진행해주세요.")
+            msgBox.exec()
+            return
         #아이디 중복 체크
         connection = self.dbConnect()
         result = self.dbSearch(query=f"SELECT COUNT(*) FROM T_CHATGPT_USER WHERE USER_ID='{id}'", connection=connection)
@@ -129,18 +152,23 @@ class gpt():
             msgBox = QMessageBox()
             msgBox.setText("회원가입에 성공하였습니다.")
             msgBox.exec()
-            ui.stackedWidget_main.setCurrentIndex(0);
-    #이메일 인증
-    def generate_random_string(self,length):
-        return ''.join(str(random.randrange(0, 9)) for _ in range(length))
+            self.resetSignup()
+            ui.stackedWidget_main.setCurrentIndex(0)
+    def resetSignup(self):
+        self.id_use_yn = False
+        self.email_valid = False
+        ui.label_auth_chk.setText("인증 미확인")
+        ui.label_auth_chk.setStyleSheet("#000000")
+        ui.lineEdit_sign_id.clear()
+        ui.lineEdit_sign_email.clear()
+        ui.lineEdit_sign_pwd.clear()
+        ui.lineEdit_sign_email_key.clear()
+    # 이메일 발송 함수
     def send_email(self,recipient, subject, message):
-        print(recipient)
-        print(subject)
-        print(message)
-        smtp_server = 'smtp.naver.com'  # 사용하는 메일 서비스에 따라 변경할 수 있습니다.
+        smtp_server = 'smtp.gmail.com'  # 사용하는 메일
         smtp_port = 587
-        sender_email = 'dldudgjs31@naver.com'  # 본인의 이메일 주소로 변경하세요.
-        sender_password = 'qwqw1004[]'  # 본인의 이메일 비밀번호로 변경하세요.
+        sender_email = 'asdudgjs@knou.ac.kr'  # 본인의 이메일
+        sender_password = 'vuyhodtrxjkyzfzp'  # 본인의 이메일
         recipient = recipient+'@knou.ac.kr'
         msg = MIMEMultipart()
         msg['From'] = sender_email
@@ -159,6 +187,8 @@ class gpt():
             print('이메일 인증 코드를 해당 이메일로 전송하였습니다.')
         except Exception as e:
             print("이메일 전송 중 오류가 발생했습니다:", e)
+
+    #인증 메일 발송 버튼 클릭시
     def emailAuth(self):
         email = ui.lineEdit_sign_email.text()
         if ui.lineEdit_sign_email.text() == '':
@@ -168,30 +198,46 @@ class gpt():
             print('이메일 계정을 입력해주세요.')
             return
         try:
-            self.verification_code = self.generate_random_string(6)
+            #난수 6자리 생성
+            self.verification_code = ''.join(str(random.randrange(0, 9)) for _ in range(6))
+            #인증 메일 발송
             self.send_email(recipient=email, subject='[verification]한국방송통신대학교 이메일 인증코드',
                    message=f'한국방송통신대학교 학생인지 인증을 위한 이메일입니다.\n당신의 이메일 인증 코드는 다음과 같습니다 : {self.verification_code}')
         except Exception as e:
             print(e)
+    #메일 인증 확인
     def confirmEmailAuth(self):
         input_code = ui.lineEdit_sign_email_key.text()
-        if input_code == self.verification_code:
+        if input_code =='':
+            msgBox = QMessageBox()
+            msgBox.setText("인증 번호를 입력하세요.")
+            msgBox.exec()
+            return
+        elif input_code == self.verification_code and input_code !='':
             msgBox = QMessageBox()
             msgBox.setText("이메일 인증에 성공하였습니다.")
             msgBox.exec()
             ui.label_auth_chk.setText('인증성공')
+            ui.label_auth_chk.setStyleSheet("color: #088A08")
+            self.email_valid = True
             print('이메일 인증에 성공하였습니다.')
         else:
             msgBox = QMessageBox()
             msgBox.setText("이메일 인증에 실패하였습니다.")
             msgBox.exec()
             ui.label_auth_chk.setText('인증실패')
+            ui.label_auth_chk.setStyleSheet("color: #FF0000")
+            self.email_valid = False
             print('이메일 인증에 실패하였습니다.')
+            return
     # 로그인 진행
     def login(self):
         id = ui.lineEdit_id.text()
         pwd = ui.lineEdit_pwd.text()
         if id =='' or pwd =='':
+            msgBox = QMessageBox()
+            msgBox.setText("아이디/패스워드를 입력해주세요.")
+            msgBox.exec()
             return
         connection = self.dbConnect()
         result = self.dbSearch(
@@ -277,6 +323,7 @@ class gpt():
         )
         message = response.choices[0].text
         return message
+
     # 오류 해결사 함수
     def errorSolution(self):
         print("error")
@@ -432,52 +479,143 @@ class gpt():
             ui.plainTextEdit_func_sol.clear()
             ui.plainTextEdit_func_sol.appendPlainText(str(e))
 
-        def registerDB(self):
-            host = "my8002.gabiadb.com"
-            user = "amsdb"
-            password = "manager123!@"
-            database = "amsdb"
-            # self.db_host = ui.lineEdit_queryadv_hosturl
-            # self.db_user = ui.lineEdit_queryadv_user
-            # self.db_pwd = ui.lineEdit_queryadv_pwd
-            # self.db_database = ui.lineEdit_queryadv_database
-            connection = self.dbConnect()
-            # connection = self.dbConnect_queryadv(host=self.db_host,user=self.db_user,pwd=self.db_pwd,database=self.db_database)
+    #쿼리메이커_advance 콤보박스 테이블 조회
+    def setCombo(self):
+        # db 정보 유효성 검사
+        if ui.lineEdit_queryadv_hosturl.text()=='':
+            msgBox = QMessageBox()
+            msgBox.setText("hosturl을 입력하세요.")
+            msgBox.exec()
+            return
+        elif ui.lineEdit_queryadv_user.text()=='':
+            msgBox = QMessageBox()
+            msgBox.setText("user을 입력하세요.")
+            msgBox.exec()
+            return
+        elif ui.lineEdit_queryadv_pwd.text() == '':
+            msgBox = QMessageBox()
+            msgBox.setText("pwd을 입력하세요.")
+            msgBox.exec()
+            return
+        elif ui.lineEdit_queryadv_database.text() == '':
+            msgBox = QMessageBox()
+            msgBox.setText("database를 입력하세요.")
+            msgBox.exec()
+            return
+        self.db_host = ui.lineEdit_queryadv_hosturl.text()
+        self.db_user = ui.lineEdit_queryadv_user.text()
+        self.db_pwd = ui.lineEdit_queryadv_pwd.text()
+        self.db_database = ui.lineEdit_queryadv_database.text()
 
-            result = self.dbSearch(
-                query=f"SELECT table_name,column_name, data_type FROM information_schema.`COLUMNS` WHERE TABLE_SCHEMA = 'amsdb' and (TABLE_NAME  ='a_class' or TABLE_NAME ='a_member')",
-                connection=connection
-            )
-            print(result)
-            prompt = f"mysql로된 db의 테이블이름,컬럼명, 데이터 타입을 정의한 정보는 {result}.이 정보를 토대로 클래스별 멤버 리스트를 뽑는 쿼리 작성해줘."
+        # db 테이블 리스트 정보 불러오는 쿼리
+        connection = self.dbConnect_queryadv(host=self.db_host,user=self.db_user,pwd=self.db_pwd,database=self.db_database)
+        result = self.dbSearch(
+            query=f"SHOW TABLES FROM amsdb where tables_in_amsdb like 'T%'",
+            connection=connection
+        )
+        tables = list()
+        for x in result:
+            tables.append(x[0])
+        ui.comboBox_query_table.clear()
+        ui.comboBox_query_table_2.clear()
+        ui.comboBox_query_table.addItems(tables)
+        ui.comboBox_query_table_2.addItems(tables)
+
+    #쿼리메이커_advance
+    def queryAdvance(self):
+        print("query_adv")
+        # 유효성 체크
+        if self.validSetting():
+            return
+
+        #유효성 체크 2) db정보 입력 여부 및 테이블 조회 여부
+        host = "my8002.gabiadb.com"
+        user = "amsdb"
+        password = "manager123!@"
+        database = "amsdb"
+        if ui.lineEdit_queryadv_hosturl.text()=='':
+            msgBox = QMessageBox()
+            msgBox.setText("hosturl을 입력하세요.")
+            msgBox.exec()
+            return
+        elif ui.lineEdit_queryadv_user.text()=='':
+            msgBox = QMessageBox()
+            msgBox.setText("user을 입력하세요.")
+            msgBox.exec()
+            return
+        elif ui.lineEdit_queryadv_pwd.text() == '':
+            msgBox = QMessageBox()
+            msgBox.setText("pwd을 입력하세요.")
+            msgBox.exec()
+            return
+        elif ui.lineEdit_queryadv_database.text() == '':
+            msgBox = QMessageBox()
+            msgBox.setText("database를 입력하세요.")
+            msgBox.exec()
+            return
+        elif ui.comboBox_query_table.currentText()=='':
+            msgBox = QMessageBox()
+            msgBox.setText("테이블 선택을 해주세요.")
+            msgBox.exec()
+            return
+
+
+        # 유효성 체크 3) 쿼리 요청 사항 여부
+        query = ui.plainTextEdit_query_request.toPlainText()
+        if self.validCheck(text=query, type="쿼리 요청사항"):
+            return
+
+        self.db_host = ui.lineEdit_queryadv_hosturl.text()
+        self.db_user = ui.lineEdit_queryadv_user.text()
+        self.db_pwd = ui.lineEdit_queryadv_pwd.text()
+        self.db_database = ui.lineEdit_queryadv_database.text()
+        #connection = self.dbConnect()
+        connection = self.dbConnect_queryadv(host=self.db_host,user=self.db_user,pwd=self.db_pwd,database=self.db_database)
+
+        #step1) DB 스키마 정보 조회
+        result = self.dbSearch(
+            query=f"SELECT table_name,column_name, data_type FROM information_schema.`COLUMNS` WHERE TABLE_SCHEMA = 'amsdb' and (TABLE_NAME  ='{ui.comboBox_query_table.currentText()}' or TABLE_NAME='{ui.comboBox_query_table_2.currentText()}')",
+            connection=connection
+        )
+        print(result)
+
+        # step2) DB 스키마 정보와 함께 조회 요청사항 chat gpt에게 쿼리 요청
+        query_request = ui.plainTextEdit_query_request.toPlainText()
+        try:
+            prompt = f"mysql로된 db의 테이블이름,컬럼명, 데이터 타입을 정의한 정보는 {result}.이 정보를 토대로 {query_request}를 뽑는 쿼리 작성해줘."
             answer_learning = self.chat_gpt_content(prom=prompt, model=self.chatgpt_api_model)
             answer_learning = str(answer_learning).replace(";", "")
-            print(answer_learning)
-            connection = self.dbConnect()
-            result = self.dbSearch(
-                query=answer_learning,
-                connection=connection
-            )
-            print(result)
-            prompt2 = f"위에 알려준 테이블 정보를 토대로 클래스별 학생 리스트를 뽑는 쿼리"
-            # answer_order = self.chat_gpt_content(prom=prompt2,model=self.chatgpt_api_model)
-            # print(answer_order)
-            # ui.tableWidget_queryadv_result.
-            data = result
-            ui.tableWidget_queryadv_result.setRowCount(len(data))
-            ui.tableWidget_queryadv_result.setColumnCount(2)  # 테이블은 3개의 열로 구성
+            # chat gpt 로깅
+            self.logging(prompt=prompt, result=answer_learning)
+        except Exception as e:
+            print(e)
+            ui.plainTextEdit_query_execute.clear()
+            ui.plainTextEdit_query_execute.setPlainText(str(e))
 
-            for row_index, row_data in enumerate(data):
-                for col_index, cell_data in enumerate(row_data):
+        # step3) chat gpt가 반환해준 query로 db에 재조회
+        print(answer_learning)
+        ui.plainTextEdit_query_execute.clear()
+        ui.plainTextEdit_query_execute.setPlainText(answer_learning)
+        connection = self.dbConnect()
+        result = self.dbSearch(
+            query=answer_learning,
+            connection=connection
+        )
+        print(result)
+
+        # step4) 재조회된 db 조회 결과를 tablewidget에 추가함
+        ui.tableWidget_query_result.clear()
+        data = result
+        row_count = len(data)
+        if row_count > 0:
+            col_count = len(data[0])
+            ui.tableWidget_query_result.setRowCount(row_count)
+            ui.tableWidget_query_result.setColumnCount(col_count)
+            for row_idx, row_data in enumerate(data):
+                for col_idx, cell_data in enumerate(row_data):
                     item = QTableWidgetItem(str(cell_data))
-                    ui.tableWidget_queryadv_result.setItem(row_index, col_index, item)
+                    ui.tableWidget_query_result.setItem(row_idx, col_idx, item)
 
-                    # 헤더 레이블 설정
-            ui.tableWidget_queryadv_result.setHorizontalHeaderLabels(["학생명", "클래스명"])
-
-            # 테이블 크기에 따라 셀 크기 조절
-            ui.tableWidget_queryadv_result.resizeColumnsToContents()
-            ui.tableWidget_queryadv_result.resizeRowsToContents()
     #로그 남기기
     def logging(self, prompt,result):
         connection = self.dbConnect()
@@ -513,15 +651,22 @@ if __name__ == "__main__":
 
     ##chat-gpt api key 등록
     ui.pushButton_save_api.clicked.connect(bot.saveApiKey)
+
     ##오류 해결사) 오류 분석
     ui.radioButton_err_java.setChecked(True)
     ui.pushButton_err_sol.clicked.connect(bot.errorSolution)
+
     ##쿼리 메이커) 쿼리 생성
     ui.radioButton_qy_ora.setChecked(True)
     ui.pushButton_query.clicked.connect(bot.querySolution)
+
     ##함수 메이커) 함수 생성
     ui.radioButton_func_java.setChecked(True)
     ui.pushButton_func_make.clicked.connect(bot.functionSolution)
+
+    ##쿼리 메이커_adv) 쿼리 결과
+    ui.pushButton_query_execute.clicked.connect(bot.queryAdvance)
+    ui.pushButton_query_table_setting.clicked.connect(bot.setCombo)
 
     MainWindow.show()
     sys.exit(app.exec())
